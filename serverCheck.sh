dd09d29174b74c6fabac95cdfc7e52db0ae72711
Ben Conway <benrconway84@gmail.com>
2018-03-16 14:27:41 +0000

    Testing alternate pipescript

--- serverCheck.sh
+++ serverCheck.sh
@@ -2,7 +2,7 @@
 
 RUNNING=1
 while [  $RUNNING -eq 1 ]; do
-    ${DOCKER}/Contents/Resources/bin/docker container ls -f status=running | grep -q -e $1
+    ${DOCKER}/Contents/Resources/bin/docker container ls -f status=running | grep -q -e npm_script_test
     RUNNING=$?
     if [ $RUNNING -eq 1 ]; then
         echo "Waiting for container to run"
